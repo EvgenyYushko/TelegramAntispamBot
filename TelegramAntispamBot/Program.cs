f72c8c94e2ae24e57d3c1f74be83ef8f47a44341
Evgeny Yushko <yushkoevgeny@gmail.com>
2024-12-11 11:26:44 +0300

    Update Program.cs

--- TelegramAntispamBot/Program.cs
+++ TelegramAntispamBot/Program.cs
@@ -7,7 +7,7 @@ namespace TelegramAntispamBot
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();///3
+			CreateHostBuilder(args).Build().Run();///4
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
