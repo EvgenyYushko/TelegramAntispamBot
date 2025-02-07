63d331c72f014ae0833cabdbe34a02d3e5189ec7
Evgeny Yushko <yushkoevgeny@gmail.com>
2025-02-07 13:56:59 +0300

    Update refresh-db.sh

--- scripts/refresh-db.sh
+++ scripts/refresh-db.sh
@@ -11,46 +11,47 @@ DB_INFO=$(curl -s -X GET "https://api.render.com/v1/postgres/$RENDER_SERVICE_ID"
   -H "accept: application/json" \
   -H "authorization: Bearer $RENDER_API_KEY")
 
-# Проверка успешности запроса
+# Шаг 2: Проверка успешности запроса
 if [ -z "$DB_INFO" ]; then
   echo "❌ Ошибка: Не удалось получить данные БД."
   exit 1
 fi
 
-# Шаг 1: Получение данных подключения к БД
+# Шаг 3: Получение конфиденциальных данных подключения к БД
 echo "🔄 Получение данных подключения к БД..."
 CONNECTION_INFO=$(curl -s -X GET "https://api.render.com/v1/postgres/$RENDER_SERVICE_ID/connection-info" \
   -H "accept: application/json" \
   -H "authorization: Bearer $RENDER_API_KEY")
 
-# Проверка успешности запроса
+# Шаг 4: Проверка успешности запроса
 if [ -z "$CONNECTION_INFO" ]; then
   echo "❌ Ошибка: Не удалось получить данные подключения."
   exit 1
 fi
 
-# Извлечение параметров подключения
+# Шаг 5: Извлечение параметров подключения
 DB_NAME=$(echo "$DB_INFO" | jq -r '.databaseName')
 DB_PORT=5432  # Порт PostgreSQL по умолчанию
 DB_USER=$(echo "$DB_INFO" | jq -r '.databaseUser')
 DB_HOST=$(echo "$CONNECTION_INFO" | jq -r '.externalConnectionString' | awk -F':' '{print $1}')
 DB_PASSWORD=$(echo "$CONNECTION_INFO" | jq -r '.password')
 
-echo "DB_NAME=$DB_NAME DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD"
-
-# Проверка наличия всех данных
+# Шаг 6: Проверка наличия всех данных
 if [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ]; then
   echo "❌ Ошибка: Не удалось извлечь все параметры подключения."
+  echo "DB_NAME=$DB_NAME DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD"
   echo "CONNECTION_INFO: $CONNECTION_INFO"
+  echo "DB_INFO: $DB_INFO"
   exit 1
 fi
 
 echo "✅ Данные подключения получены:"
 echo "DB_NAME=$DB_NAME DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD"
 
-# Шаг 2: Создание бекапа
+# Шаг 7: Создание бекапа
 echo "🔄 Создание бекапа..."
-#PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -Fc -f $BACKUP_FILE
+PGPASSWORD=$DB_PASSWORD 
+pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -Fc -f $BACKUP_FILE
 
 if [ $? -eq 0 ]; then
   echo "✅ Бекап успешно создан: $BACKUP_FILE"
