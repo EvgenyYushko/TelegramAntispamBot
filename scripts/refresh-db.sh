36d68ac9f8ab59a881c3d4e0e77af069aaeb2f80
Evgeny Yushko <yushkoevgeny@gmail.com>
2025-02-07 10:16:50 +0300

    Update refresh-db.sh

--- scripts/refresh-db.sh
+++ scripts/refresh-db.sh
@@ -2,14 +2,19 @@
 
 # Конфигурация
 BACKUP_FILE="backup.dump"
+# Если вы задаёте пароль напрямую в скрипте (хотя лучше использовать переменную окружения):
 RENDER_API_KEY="rnd_sZLs5c8GIjjEmSc7EwblTKTvoTLZ"
 RENDER_SERVICE_ID="dpg-cu365mt2ng1s73c6t8b0-a"
 
 # Шаг 1: Получение данных текущей БД
 DB_INFO=$(curl -s -X GET "https://api.render.com/v1/services/$RENDER_SERVICE_ID/env-vars" \
-  -H "Authorization: Bearer $RENDER_API_KEY" | jq -r '.[] | select(.key | endswith("DATABASE_URL")) | .value')
+  -H "Authorization: Bearer $RENDER_API_KEY" | jq -r '.envVars[] | select(.key | endswith("DATABASE_URL")) | .value')
 
-# Извлечение параметров подключения
+# Выведем полученный DB_INFO для отладки
+echo "DB_INFO: $DB_INFO"
+
+# Извлечение параметров подключения из строки, например:
+# postgres://username:password@host:port/database
 DB_HOST=$(echo $DB_INFO | awk -F'@' '{print $2}' | cut -d':' -f1)
 DB_PORT=$(echo $DB_INFO | awk -F':' '{print $4}' | cut -d'/' -f1)
 DB_USER=$(echo $DB_INFO | awk -F':' '{print $2}' | cut -d'/' -f3)
@@ -18,6 +23,6 @@ DB_NAME=$(echo $DB_INFO | awk -F'/' '{print $4}')
 
 echo "DB_NAME=$DB_NAME DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD"
 
-# Шаг 2: Создание бекапа
+# Шаг 2: Создание бэкапа
 echo "🔄 Creating backup..."
 PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -Fc -f $BACKUP_FILE
