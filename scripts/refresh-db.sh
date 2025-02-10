fa04d34be065cec1c6f0f91aa7bdf82703c9a036
Evgeny Yushko <yushkoevgeny@gmail.com>
2025-02-10 12:42:55 +0300

    Update refresh-db.sh

--- scripts/refresh-db.sh
+++ scripts/refresh-db.sh
@@ -3,11 +3,11 @@
 # Конфигурация
 BACKUP_FILE_NAME="backup.dump"
 RENDER_API_KEY="rnd_sZLs5c8GIjjEmSc7EwblTKTvoTLZ"
-DB_ID="dpg-cukr4htds78s739rak70-a"
+DB_ID="dpg-cuks7g2n91rc73auquf0-a"
 WEB_SERVICE_ID="srv-ctaoq5hu0jms73f1l3q0"
 
-NEW_DB_NAME="telergamdb12"
-NEW_DB_USER="telergamdb_user12"
+NEW_DB_NAME="telergamdb13"
+NEW_DB_USER="telergamdb_user13"
 
 # Функция для гарантированного запуска сервиса при ошибке
 trap 'handle_error' ERR
@@ -176,9 +176,14 @@ fi
 
 NEW_DB_PASSWORD=$(echo "$CONNECTION_NEW_DB_INFO" | jq -r '.password')
 
+psql -h "$NEW_DB_ID.oregon-postgres.render.com" \
+     -p 5432 -U "$NEW_DB_USER" \
+     -d "$NEW_DB_NAME" \
+     -c "CREATE ROLE $NEW_DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';"
+
 export PGPASSWORD=$NEW_DB_PASSWORD
 #pg_restore -h "$NEW_DB_ID.oregon-postgres.render.com" -p 5432 -U $NEW_DB_USER -d $NEW_DB_NAME backup.dump
-pg_restore -h "$NEW_DB_ID.oregon-postgres.render.com" -p 5432 -U $NEW_DB_USER --no-owner --create -d $NEW_DB_NAME backup.dump
+pg_restore -h "$NEW_DB_ID.oregon-postgres.render.com" -p 5432 -U $NEW_DB_USER --create -d $NEW_DB_NAME backup.dump
 
 echo "🚀 Starting web service..."
 curl -X POST "https://api.render.com/v1/services/$WEB_SERVICE_ID/resume" \
